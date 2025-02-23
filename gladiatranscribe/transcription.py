887bf4cd2e4773f8be87e2ccd10c753307538d54
Conan747 <conan747@users.noreply.github.com>
2025-02-23 18:51:45 -0300

    First working version.

--- gladiatranscribe/transcription.py
+++ gladiatranscribe/transcription.py
@@ -1,11 +1,10 @@
 import asyncio
-from io import BytesIO
 from typing import List, Type
 
 import aiohttp
 import mautrix.crypto.attachments
 from maubot import Plugin, MessageEvent
-from maubot.handlers import command, event
+from maubot.handlers import event
 from mautrix.client import Client as MatrixClient
 from mautrix.types import MessageType, EventType, MediaMessageEventContent, EncryptedFile
 from mautrix.util.config import BaseProxyConfig
@@ -49,12 +48,10 @@ class GladiaTranscribe(Plugin):
     config: Config
 
     polls: List[TranscriptionPoll] = []
-    _cancellation_token: asyncio.CancellationToken
     _poll_task: asyncio.Task   
 
     async def start(self) -> None:
         self.config.load_and_update()
-        self._cancellation_token = asyncio.CancellationToken()
         self._poll_task = None
 
     @event.on(EventType.ROOM_MESSAGE)
@@ -67,7 +64,8 @@ class GladiaTranscribe(Plugin):
             return
 
         content: MediaMessageEventContent = evt.content
-        self.log.debug("A voice message was received.")
+        mime = content.info.mimetype
+        self.log.debug(F"A voice message was received with mimetype {mime}")
 
         if content.url:  # content.url exists. media is not encrypted
             data = await download_unencrypted_media(content.url, evt.client)
@@ -77,31 +75,39 @@ class GladiaTranscribe(Plugin):
             self.log.warning("A message with type audio was received, but no media was found.")
             return
         
-        audio_url = await self.upload_audio(self, data)
+        audio_url = await self.upload_audio(data, mime)
+        if not audio_url:
+            return
         transcription_url = await self.request_transcription(audio_url)
+        if not transcription_url:
+            self.log.debug("No transcription url")
+            return
         self.polls.append(TranscriptionPoll(transcription_url, evt))
         self.start_poll_task()
 
 
-    async def upload_audio(self, data: bytes) -> str:
+    async def upload_audio(self, data: bytes, mime: str) -> str:
         """
         Uploads the audio file to the transcription API and sends the transcription as a reply.
         """
         # Header for API KEY
         self.http.headers["x-gladia-key"] = self.config['api_key']
-        self.http.headers["Content-Type"] = "multipart/form-data"
 
         # initialize data for the POST request
         request_data = aiohttp.FormData()
+        request_data.add_field(
+                name="audio",  # Field name expected by the API
+                value=data,
+                filename="audio.ogg",  # Filename for the audio file
+                content_type=mime  # MIME type for binary data
+            )
 
-        # Convert data bytes into a BytesIO object
-        request_data.add_field('audio', BytesIO(data))
         response = await self.http.post(UPLOAD_URL, data=request_data)
+        response_json = await response.json()
         # Properly log if the response is not what expected
         if response.status != 200:
-            self.log.error(F"Failed to upload audio: {response}")
+            self.log.error(F"Failed to upload audio:\n{response_json}")
             return
-        response_json = await response.json()
         audio_url = response_json['audio_url']
         self.log.debug(F"Audio uploaded to {audio_url}")
         return audio_url
@@ -112,16 +118,17 @@ class GladiaTranscribe(Plugin):
         """
         self.http.headers["x-gladia-key"] = self.config['api_key']
         # initialize data for the POST request
-        request_data = aiohttp.FormData()
-        request_data.add_field('audio_url', audio_url)
-        request_data.add_field('enable_code_switching', True)
-        request_data.add_field('code_switching_config', {"languages": self.config['languages']})
-
+        request_data = aiohttp.JsonPayload({
+            "audio_url": audio_url,
+            "enable_code_switching": True,
+            "code_switching_config": {"languages": self.config['languages']}
+        })
         response = await self.http.post(TRANSCRIPTION_URL, data=request_data)
-        if response.status != 200:
-            self.log.error(F"Failed to request transcription: {response}")
-            return
         response_json = await response.json()
+        
+        if response.status != 201:
+            self.log.error(F"Failed to request transcription:\n{response_json}")
+            return
         transcription_url = response_json['result_url']
         self.log.debug(F"Transcription requested: {transcription_url}")
         return transcription_url
@@ -130,31 +137,31 @@ class GladiaTranscribe(Plugin):
         """
         Polls the transcription API for the transcriptions of the uploaded audio files.
         """
-        while not self._cancellation_token.is_cancelled():
+        while len(self.polls) > 0:
             try:
                 newPolls = []
                 for poll in self.polls:
                     self.http.headers["x-gladia-key"] = self.config['api_key']
                     response = await self.http.get(poll.url)
-
+                    if response.status != 200:
+                        self.log.debug(F"Failed to poll transcription: {response}")
+                        newPolls.append(poll)
+                        continue
                     response_json = await response.json()
                     if response_json['status'] == 'done':
-                        await self.use_transcription(poll, response_json['result']['transcription'])
+                        await self.use_transcription(poll, response_json['result']['transcription']['full_transcript'])
                     else:
                         newPolls.append(poll)
                         continue
                 self.polls = newPolls
-                if len(self.polls) == 0:
-                    self._cancellation_token.cancel()
-                    break
-                await asyncio.sleep(self.config["poll_interval"], lookup=self._cancellation_token) # Sleep and check cancellation during sleep
+                await asyncio.sleep(self.config["poll_interval"])
             except asyncio.CancellationError:
                 # Ignore CancellationError during sleep - this is expected when task is stopped
                 pass
             except Exception as e:
-                print(f"Error in periodic task: {e}")
+                self.log.debug(f"Error in periodic task: {e}")
                 # Optionally handle errors here, maybe log them or implement retry logic
-        print("Periodic task stopped.") # Indicate task has stopped gracefully
+        self.log.debug("Periodic task stopped.") # Indicate task has stopped gracefully
 
     async def use_transcription(self, poll: TranscriptionPoll, transc: str):
         """
@@ -163,21 +170,17 @@ class GladiaTranscribe(Plugin):
         self.log.debug(F"Message transcribed: {transc}")
 
         # send transcription as reply
-        await poll.evt.reply(transc)
-        self.log.debug("Reply sent")
+        id = await poll.evt.reply(F"Automatic transcription:\n{transc}")
+        self.log.debug(F"Reply sent with ID {id}")
 
         
     def start_poll_task(self):
         """Starts the periodic task in a separate task."""
-        if (
-            (self._cancellation_token is not None and not self._cancellation_token.cancelled())
-              or (self._poll_task is not None and not self._poll_task.done())
-              ):
-            print("Periodic task already running.")
+        if self._poll_task is not None and not self._poll_task.done():
+            self.log.debug("Periodic task already running.")
             return
-        self._cancellation_token = asyncio.CancellationToken()
         self._poll_task = self.loop.create_task(self._poll_transcriptions())
-        print("Periodic task started.")
+        self.log.debug("Periodic task started.")
 
     @classmethod
     def get_config_class(cls) -> Type[BaseProxyConfig]:
